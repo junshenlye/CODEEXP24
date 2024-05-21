6ab48c4a3ccb6ac55ad96910611e4f55b9b3edfb
junshenlye <junshenlye@gmail.com>
2024-05-21 15:30:02 +0800

    added standard styles file

--- src/screens/SplashScreen/SplashScreen.js
+++ src/screens/SplashScreen/SplashScreen.js
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import globalStyles from '../../styles/globalStyles';
 
 export default function SplashScreen({ navigation }) {
   return (
-    <View style={styles.container}>
+    <View style={globalStyles.container}>
       <Text>Hello World!</Text>
       <Button title="Click Here" onPress={() => navigation.navigate("Login")} />
     </View>
